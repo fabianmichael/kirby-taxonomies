8d80d165a0ffa1a7ae199c6d289c581b8950f6de
Fabian Michael <hallo@fabianmichael.de>
2025-06-29 17:55:41 +0200

    feat: add option to get all pages with given category

--- src/BlockModels/Category.php
+++ src/BlockModels/Category.php
@@ -4,6 +4,7 @@ namespace FabianMichael\Taxonomies\BlockModels;
 
 use Kirby\Cms\Block;
 use Kirby\Cms\Page;
+use Kirby\Cms\Pages;
 use Kirby\Content\Field;
 
 class Category extends Block
@@ -42,5 +43,19 @@ class Category extends Block
 		return $parent->url([
 			'params' => [t('taxonomies.category.slug', 'category') => $this->slug(),
 		], ]);
+	}
+
+	public function items(): Pages
+	{
+		/** @var Page */
+		$parent = $this->parent();
+
+		return $parent->children()->listed()
+			->filterBy('categories', $this->id(), ',');
+	}
+
+	public function count(): int
+	{
+		return $this->items()->count();
 	}
 }
